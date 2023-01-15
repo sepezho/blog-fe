5da86f21a6c5bbcbd3442500475856a58b899e0c
sepezho ⠀ <sepezho@sepezhos-MacBook-Pro.local>
2023-01-15 23:45:27 +0100

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReactMarkdown from 'react-markdown'
+import styled from 'styled-components'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blog";
+import NoPage from "./pages/NoPage";
 
 function App() {
-  const text = `
-# some *sample* 
-# *text*`
 
   return (
     <div className="App">
       <header className="App-header">
-        <ReactMarkdown children={text} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="blog" element={<Blogs />} />
+              <Route path="*" element={<NoPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
