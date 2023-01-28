594a01dc22c89b2608dba3f83c4684e8271747c0
sepezho ⠀ <sepezho@sepezhos-MacBook-Pro.local>
2023-01-29 00:54:14 +0100

    added sepezho.log posts execution

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,10 +5,18 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import { Link } from "react-router-dom";
 dayjs.extend(relativeTime);
 
+import ReactMarkdown from 'react-markdown'
+
+import rehypeRaw from 'rehype-raw'
+import remarkGfm from 'remark-gfm'
 const ListTexts = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  p {
+    width: 700px;
+    max-width: 700px;
+  }
   button {
     width: 700px;
     margin: 4px 0;
@@ -16,6 +24,7 @@ const ListTexts = styled.div`
     cursor: pointer;
   }
   margin: 0 0 16px 0;
+  margin: auto;
 `
 
 const pagesInit = [
@@ -27,24 +36,27 @@ const pagesInit = [
 ]
 
 const Home = () => {
-  const [pages, setPages] = useState(pagesInit)
+  const [pages, setPages] = useState('')
   const [error, setError] = useState('')
 
   useEffect(() => {
-    fetch('https://api.blog.sepezho.com:4646/api/list').then(e => e.json()).then(e => {
+    fetch(`${process.env.REACT_APP_MODE === 'dev' ? 'http://localhost:4646' : 'https ://api.blog.sepezho.com:4646'}/api/list`).then(e => e.json()).then(e => {
       console.log(e)
-      setPages(e.data.reverse())
+      setPages(e.data.reverse().reduce((p: string, n: any) => {
+
+        return p + '-----------------' + n.Title + '<br/>' + n.Text + "<br /><br />"
+      }, ''))
     }).catch(e => {
       setError(`load error: ${e}`)
     })
   }, [])
 
-  return <div>
-    <ListTexts>
-      <h1>Posts</h1>
-      {error ? error : pages.map(e => (<Link to={`/post/${e.Id}`}><button key={e.Id}> {`#${e.Id} ||| ${dayjs(e.Date).fromNow()} ||| ${e.Title}`} </ button></Link>))}
-    </ListTexts>
-  </div >;
+  return <ListTexts>
+    <h1>Posts</h1>
+    <ReactMarkdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]} children={pages} />
+
+  </ListTexts>
 };
+// {error ? error : pages.map(e => (<Link to={`/post/${e.Id}`}><button key={e.Id}> {`#${e.Id} ||| ${dayjs(e.Date).fromNow()} ||| ${e.Title}`} </ button></Link>))}
 
 export default Home;
