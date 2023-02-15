3900c9faf3709bd783743df32ddb186c7f0f50ff
sepezho ⠀ <sepezho@sepezhos-MacBook-Pro.local>
2023-02-15 22:22:12 +0100

    audio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,109 @@ import React, { useRef, useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { Link } from "react-router-dom";
-
+import VideoJS from 'video.js'
 import ReactMarkdown from 'react-markdown'
 
 import rehypeRaw from 'rehype-raw'
 import remarkGfm from 'remark-gfm'
 dayjs.extend(relativeTime);
+interface VideoControlsProps {
+  src: string;
+}
+
+function VideoControls({ src }: VideoControlsProps) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [volume, setVolume] = useState<number>(1);
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [duration, setDuration] = useState<number>(0);
+
+  function handlePlayPauseClick(): void {
+    if (videoRef.current?.paused) {
+      videoRef.current.play();
+      setIsPlaying(true);
+    } else {
+      if (videoRef.current) {
+        videoRef.current.pause();
+        setIsPlaying(false);
+      }
+    }
+  }
+
+  function handleVolumeChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+    if (videoRef.current) {
+      videoRef.current.volume = newVolume;
+    }
+  }
+
+  function handleTimeChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const newTime = parseFloat(event.target.value);
+    setCurrentTime(newTime);
+    if (videoRef.current) {
+      videoRef.current.currentTime = newTime;
+    }
+  }
+
+  function handleTimeUpdate(): void {
+    if (videoRef.current) {
+      setCurrentTime(videoRef.current.currentTime);
+    }
+  }
+
+  function handleDurationChange(): void {
+    if (videoRef.current) {
+      setDuration(videoRef.current.duration);
+    }
+  }
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.addEventListener("timeupdate", handleTimeUpdate);
+      videoRef.current.addEventListener("durationchange", handleDurationChange);
+    }
+    return () => {
+      if (videoRef.current) {
+        videoRef.current.removeEventListener("timeupdate", handleTimeUpdate);
+        videoRef.current.removeEventListener("durationchange", handleDurationChange);
+      };
+    };
+  }, []);
+
+  return (
+    <div className="video-container">
+      <div className="video-window">
+        <video className="video" ref={videoRef}>
+          <source src={src} type="video/mp4"></source>
+        </video>
+      </div>
+      <div className="controls-container">
+        <button className="plplay-pause-buttonay-pause-button" onClick={handlePlayPauseClick}>
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+        <input
+          className="volume-slider"
+          type="range"
+          min="0"
+          max="1"
+          step="0.1"
+          value={volume}
+          onChange={handleVolumeChange}
+        />
+        <input
+          className="time-slider"
+          type="range"
+          min="0"
+          max={duration}
+          step="0.01"
+          value={currentTime}
+          onChange={handleTimeChange}
+        />
+      </div>
+    </div>
+  );
+}
 const ListTexts = styled.div`
   display: flex;
   flex-direction: column;
@@ -23,6 +120,10 @@ const ListTexts = styled.div`
     text-align: left;
     cursor: pointer;
   }
+  img {
+
+    max-width: 400px;
+  }
   margin: 0 0 16px 0;
   margin: auto;
 `
@@ -36,49 +137,174 @@ const pagesInit = [
 ]
 
 const Home = () => {
-  const [pages, setPages] = useState('')
+  const [pages, setPages] = useState([])
   const [error, setError] = useState('')
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(0)
+  const [result, setResult] = useState([<div></div>])
 
   const ref = useRef(null)
+  const playerRef = React.useRef(null);
+
+
+  const handlePlayerReady = (player: any) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      VideoJS.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      VideoJS.log('player will dispose');
+    });
+  };
+
+  const openVideo = (e: string) => {
+    alert(e)
+  }
+
   useEffect(() => {
     fetch(`${process.env.REACT_APP_MODE === 'dev' ? 'http://localhost:4646' : 'https://api.blog.sepezho.com:4646'}/api/list?page=${page}`).then(e => e.json()).then(e => {
-      (async () => {
-
-        let result = '';
-        let i = 0;
-
-        while (i < e.data.length) {
-          const n = e.data[i];
-
-          if (n.Thumbnail) {
-            const resp = await fetch(`${process.env.REACT_APP_MODE === 'dev' ? 'http://localhost:4646' : 'https://api.blog.sepezho.com:4646'}/api/image?image=${n.Thumbnail}`)
-            const req = await resp.blob();
-            const areaImageObjectURL = URL.createObjectURL(req);
-
-            console.log(areaImageObjectURL)
-            result += '-----------------' + n.Title + '<br/>' + "<img src=" + areaImageObjectURL + " alt='' /><br />" + n.Text + "<br /><br />"
-          } else {
-            result += '-----------------' + n.Title + '<br/>' + n.Text + "<br /><br />"
-          }
-          i++;
-        }
-        if (ref.current) {
-          //@ts-ignore
-          ref.current.innerHTML = pages + result
-        }
-        setPages(pages + result)
-      })()
+      setPages(pages[0] ? [...pages, ...e.data] : e.data)
     }).catch(e => {
       setError(`load error: ${e}`)
     })
   }, [page])
 
+  const [audio] = useState(new Audio());
+  const [isPlaying, setIsPlaying] = useState(false);
+  const toggleAudio = (file: string) => {
+    console.log(isPlaying)
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.src = "http://localhost:4646/api/audio?file=" + file
+      audio.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+  useEffect(() => {
+    (async () => {
+      let resultNew = [];
+      let i = 0;
+      while (i < pages.length) {
+        const n = pages[i] as any;
+        if (n.Thumbnail) {
+          const resp = await fetch(`${process.env.REACT_APP_MODE === 'dev' ? 'http://localhost:4646' : 'https://api.blog.sepezho.com:4646'}/api/image?image=${n.Thumbnail}`)
+          const req = await resp.blob();
+          const areaImageObjectURL = URL.createObjectURL(req);
+          const videoJsOptions = {
+            autoplay: true,
+            controls: true,
+            responsive: true,
+            fluid: true,
+            sources: [{
+              src: "http://localhost:4646/api/video?file=" + n.Video,
+              type: 'video/mp4'
+            }]
+          };
+          // <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+          console.log(n.Video
+          )
+
+          if (n.Video.includes('video_files')) {
+            resultNew.push(
+              <>
+                -----------------
+                {n.Title}
+                <br />
+                {n.Date}
+                <br />
+                <video className="video" controls>
+                  <source src={"http://localhost:4646/api/video?file=" + n.Video} type="video/mp4"></source>
+                </video>
+                <br />
+                {n.Text}
+                <br />
+                <br />
+              </>
+            )
+          } else {
+            resultNew.push(
+              <>
+                -----------------
+                {n.Title}
+                <br />
+                {n.Date}
+                <br />
+                <VideoControls src={"http://localhost:4646/api/video?file=" + n.Video} />
+                <br />
+                {n.Text}
+                <br />
+                <br />
+              </>
+            )
+          }
+        } else {
+          if (n.Video && n.Video.includes('voice_messages')) {
+            resultNew.push(
+              <>
+                -----------------
+                {n.Title}
+                <br />
+                {n.Date}
+                <br />
+
+                <button onClick={() => toggleAudio(n.Video)}>{isPlaying ? 'Pause audio' : 'Play audio'}</button>
+                <br />
+                {n.Text}
+                <br />
+                <br />
+              </>
+            )
+          }
+          if (n.Photo) {
+            const resp = await fetch(`${process.env.REACT_APP_MODE === 'dev' ? 'http://localhost:4646' : 'https://api.blog.sepezho.com:4646'}/api/image?image=${n.Photo}`)
+            const req = await resp.blob();
+            const areaImageObjectURL = URL.createObjectURL(req);
+            resultNew.push(
+              <>
+                -----------------
+                {n.Title}
+                <br />
+                {n.Date}
+                <br />
+                <img src={areaImageObjectURL} alt='' />
+                <br />
+                {n.Text}
+                <br />
+                <br />
+              </>
+            )
+          } else {
+            resultNew.push(
+              <>
+                -----------------
+                {n.Title}
+                <br />
+                {n.Date}
+                <br />
+                {n.Text}
+                <br />
+                <br />
+              </>
+            )
+          }
+        }
+        i++;
+      }
+      setResult(resultNew)
+    })()
+  }, [pages])
+
   return <ListTexts>
     <h1>Posts</h1>
 
-    <div ref={ref}>
-    </div>
+    <p>
+      {
+        result
+      }
+    </p>
     <button onClick={() => setPage(page + 1)}>load more</button>
   </ListTexts>
 };
